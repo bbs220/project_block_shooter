06331bbb67c10db4577896bbae49fc62a9307c8e
bbs220 <bsawant357@gmail.com>
2026-09-04 23:40:41 +0530

    new sounds

--- apps/appClient/src/utils/assetPaths.ts
+++ apps/appClient/src/utils/assetPaths.ts
@@ -14,12 +14,12 @@ export const soundBank: Record<string, string> = {
   hit: "/sounds/hit_marker.mp3",
   kill: "/sounds/kill.mp3",
   thunder: "/sounds/thunder.mp3",
-  pistolFire: "",
-  pistolReload: "",
-  assaultRifleFire: "",
-  assaultRifleReload: "",
-  burstRifleFire: "",
-  burstRifleReload: "",
+  pistolFire: "/sounds/pistol_fire.mp3",
+  pistolReload: "/sounds/pistol_reload.mp3",
+  assaultRifleFire: "/sounds/assault_rifle_fire.mp3",
+  assaultRifleReload: "/sounds/assault_rifle_reload.mp3",
+  burstRifleFire: "/sounds/burst_rifle_fire.mp3",
+  burstRifleReload: "/sounds/burst_rifle_reload.mp3",
 };
 
 export const modelsBank: Record<string, string> = {
